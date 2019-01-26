7163a6fd4d06484b931fdecbc8464577c588dbf0
Thomas Zhu <dev@thomaszhu.com>
2019-01-25 22:27:34 -0800

    First commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View>
+        
+      </View>
+    );
+  }
+}
+
+/* tslint:disable-next-line:no-default-export */
+export default App;
+/* tslint:enable */
---
+++ src/values/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+  // Add string literals (for style constants), or Color instances (for props)
+  // Color Docs: https://github.com/qix-/color#readme
+  
+};
